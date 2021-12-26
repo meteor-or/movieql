e40851b444c5125e48a9fa6137b4e093e5295370
meteor lee <82145837+meteor-or@users.noreply.github.com>
2021-12-26 23:34:10 +0900

    db 정보 변경, mutation 정의

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-import {
-  graphql,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-} from "graphql";
 import { createGraphQLServer } from "graphql-yoga";
+import { getMovies, getById } from "./db.js";
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: "Query",
-    fields: () => ({
-      ping: {
-        type: GraphQLString,
-        resolve: () => "pong",
-      },
-    }),
-  }),
-});
+//schema
+const typeDefs = `
+type Movie {
+  id: Int!
+  name: String!
+  score: Int!
+}
+
+type Query {
+  movies: [Movie]!
+  movie(id: Int!): Movie
+}
+
+type Mutation {
+  addMovie(name: String!, score: Int!): Movie!
+}
+`;
+
+//resolver
+const resolvers = {
+  Query: {
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+  },
+};
+
 // Provide your schema
 const server = createGraphQLServer({
-  schema,
-  isDev: process.env.NODE_ENV !== "production",
+  typeDefs,
+  resolvers,
 });
 // Start the server and explore http://localhost:4000/graphql
-server.start();
+server.start(() => console.log("Graphql Server Running"));
